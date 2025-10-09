c740d30f557cfacb4a9ece2ae43ea3f7ec9b2cc8
ShapikNya <Shapik_psn@mail.ru>
2025-10-09 19:58:08 +0300

    feat(domain): add core entities

--- Tasks.Backend/Tasks.Domain/Task.cs
+++ Tasks.Backend/Tasks.Domain/Task.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Tasks.Domain
 {
-    internal class Task
+    public class Task
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
